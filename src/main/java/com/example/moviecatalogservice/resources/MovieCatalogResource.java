1cea9314e3a309a2cb76c69cba496c1e861b8a56
Deepanshu <deepanshuchauhanrocky@gmail.com>
2020-03-05 17:45:45 +0530

    Communication done !!!

--- src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,7 +2,7 @@ package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
-import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/catalog")
+@RequestMapping("/catalogs")
 public class MovieCatalogResource {
 
     @Autowired
@@ -28,32 +26,26 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalogs(@PathVariable("userId") String userId) {
 
+        //get all the rating list by call the rating-data-service
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratings/user/" + userId, UserRating.class);
+        return ratings.getUserRating().stream().map(rating -> {
+            //for Each movie Id , call movie Info service and get the details
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
-        //get all the rating list
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("4567", 5)
-        );
+            // Put them all together
+            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+        })
+                .collect(Collectors.toList());
+    }
+}
+/*   return Collections.singletonList(
+                new CatalogItem("Suryavanshi", "Action", 4)
+        );*/
 
-        return ratings.stream().map(rating -> {
-            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-           Movie movie= webClientBuilder.build()
+/*Movie movie= webClientBuilder.build()
                     .get()
                     .uri("http://localhost:8082/movies/" + rating.getMovieId() )
                     .retrieve()
                     .bodyToMono(Movie.class)
                     .block();
-
-            return new CatalogItem(movie.getName(), "Description", rating.getRating());
-        })
-                .collect(Collectors.toList());
-
-        //for Each movie Id , call movie Info service and get the details
-
-        // Put them all together
-//        return Collections.singletonList(
-//                new CatalogItem("Suryavanshi", "Action", 4)
-//        );
-    }
-
-}
+            */
